b82f68c77701bc6a04ba3ffe7a88c14d8bec6915
CoconutGithub <khkoh5932@gmail.com>
2025-03-26 17:06:07 +0900

    setting MjuEcs Project

---
+++ src/main/java/org/mjuecs/mjuecs/MjuEcsApplication.java
@@ -0,0 +1,13 @@
+package org.mjuecs.mjuecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjuEcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MjuEcsApplication.class, args);
+    }
+
+}
