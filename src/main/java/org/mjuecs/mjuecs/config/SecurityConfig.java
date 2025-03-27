5278c0188e7510ad1d0ee85ba2e9fdc51a261c90
CoconutGithub <khkoh5932@gmail.com>
2025-03-28 00:46:53 +0900

    sec Config 인증 경로에서 auth경로(로그인)는 전부 통과, 이외에는 전부 인증 필요(토큰 필요) 커스텀 토클 필터 추가

--- src/main/java/org/mjuecs/mjuecs/config/SecurityConfig.java
+++ src/main/java/org/mjuecs/mjuecs/config/SecurityConfig.java
@@ -1,24 +1,34 @@
 package org.mjuecs.mjuecs.config;
 
+import lombok.RequiredArgsConstructor;
+import org.mjuecs.mjuecs.jwt.JwtFilter;
+import org.mjuecs.mjuecs.jwt.JwtUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain filter(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(
-                        (auth) -> auth.requestMatchers("/docker/**").permitAll()
-                );
-        http.csrf((auth) -> auth.disable());
-        http.httpBasic((auth) -> auth.disable());
+    private final JwtUtil jwtUtil;
 
-        return http.build();
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .httpBasic((auth->auth.disable()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 }
