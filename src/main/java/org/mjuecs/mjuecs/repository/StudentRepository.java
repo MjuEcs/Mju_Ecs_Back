21f0f6671993e2ba036aa7d5aeb109f30d336e8c
CoconutGithub <khkoh5932@gmail.com>
2025-04-03 06:44:29 +0900

    refactor

--- src/main/java/org/mjuecs/mjuecs/repository/StudentRepository.java
+++ src/main/java/org/mjuecs/mjuecs/repository/StudentRepository.java
@@ -1,10 +1,8 @@
-package org.mjuecs.mjuecs.Repository;
+package org.mjuecs.mjuecs.repository;
 
 import org.mjuecs.mjuecs.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student,String> {
