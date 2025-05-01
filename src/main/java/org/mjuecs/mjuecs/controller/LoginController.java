6b0014692f6b053dfc9da5d3fc89ddf3c32feb20
shinnk <sygys10293@gmail.com>
2025-05-01 21:28:31 +0900

    Put tokens in cookies

--- src/main/java/org/mjuecs/mjuecs/controller/LoginController.java
+++ src/main/java/org/mjuecs/mjuecs/controller/LoginController.java
@@ -2,6 +2,7 @@ package org.mjuecs.mjuecs.controller;
 
 import com.github.dockerjava.api.exception.UnauthorizedException;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.mjuecs.mjuecs.dto.LoginDto;
 import org.mjuecs.mjuecs.service.StudentService;
@@ -21,16 +22,19 @@ public class LoginController {
     private final StudentService studentService;
 
     @PostMapping("/auth/login")
-    public ResponseEntity<?> login(@RequestBody LoginDto loginDto) {
+    public ResponseEntity<?> login(@RequestBody LoginDto loginDto, HttpServletResponse response) {
         try {
             String token = studentService.login(loginDto);
 
             Cookie cookie = new Cookie("token", token);
             cookie.setMaxAge(86400000);
-            cookie.setHttpOnly(true);
+            cookie.setPath("/");
+
+            response.addCookie(cookie);
 
 //            return ResponseEntity.ok(Map.of("token", "Bearer " + token)).header(HttpHeaders.SET_COOKIE, cookie.toString());
-            return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).body(Map.of("token", "Bearer " + token));
+            return ResponseEntity.ok("Login successful");            
+            // return ResponseEntity.ok(Map.of("token", "Bearer " + token));
         } catch (UnauthorizedException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증 실패");
         }
