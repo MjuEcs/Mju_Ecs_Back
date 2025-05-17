d2791bc052061adabdfcdcf96b432ffafc5f7d3f
CoconutGithub <khkoh5932@gmail.com>
2025-05-17 10:55:40 +0900

    add scheduling

--- src/main/java/org/mjuecs/mjuecs/MjuEcsApplication.java
+++ src/main/java/org/mjuecs/mjuecs/MjuEcsApplication.java
@@ -2,7 +2,9 @@ package org.mjuecs.mjuecs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class MjuEcsApplication {
 
